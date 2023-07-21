9afc5a4c95b67c60f77d1f042070a210828fc95f
Matheus Drumond <116853507+matheusDrumond@users.noreply.github.com>
2023-07-21 13:55:15 -0300

    Update scripts.js

--- src/scripts.js
+++ src/scripts.js
@@ -1,13 +1,16 @@
+// Lógica de mostrar oue sconder a senha e trocar os ícones
 let passwordInput = document.querySelector('#password');
 let hideButton = document.querySelector('#hide');
 let viewButton = document.querySelector('#view');
 
+// Mostrar a senha
 hideButton.addEventListener('click', function(){
     passwordInput.type = 'text';
     hideButton.classList.add('hidden');
     viewButton.classList.remove('hidden');
 });
 
+// Esconder a senha
 view.addEventListener('click', function(){
     passwordInput.type = 'password';
     viewButton.classList.add('hidden');
